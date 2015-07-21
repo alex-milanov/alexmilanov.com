3704017ca8fa571c71f122d1b28782564c40fbed
Alex Milanov <alex@i4web.biz>
2015-07-21 17:53:23 +0300

    initial commit, side panel impl

---
+++ src/js/main.js
@@ -0,0 +1,37 @@
+$(function(){
+	var context = {
+		params: {}
+	};
+
+	$("body").on("click","a[class*='-toggle']",function(){
+		$(this).toggleClass("toggled");
+		var $toggleRef = $($(this).data("toggle-ref"));
+		var _toggleClass = $(this).data("toggle-class");
+		var _toggleParam = $(this).data("toggle-param");
+		$toggleRef.toggleClass(_toggleClass);
+		if(_toggleParam !== ""){
+			context.params[_toggleParam] = !context.params[_toggleParam];
+		}
+	});
+
+	$("body").on("click","a[class*='-trigger']",function(){
+		var _triggerMethod = $(this).data("trigger-method");
+		if(typeof context[_triggerMethod] !== "undefined"){
+			if($(this).data("trigger-id")){
+				context[_triggerMethod]($(this).data("trigger-id"));
+			}
+			context[_triggerMethod]();
+		}
+	});
+
+
+	$("body").on("click","a[class*='-option']",function(_ev){
+		$("a[class*='-option']").removeClass("selected");
+		$(this).addClass("selected");
+
+		var _optionParam = $(this).data("option-param");
+		var _optionValue = $(this).data("option-value");
+
+		context.params[_optionParam] = _optionValue;
+	});
+})
